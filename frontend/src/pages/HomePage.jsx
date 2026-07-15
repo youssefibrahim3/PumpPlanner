f1108229f40d0ba23a5ac263fd528a654fec8dea
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-07-14 18:07:48 -0600

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,8 +3,12 @@ import { useState, useEffect } from "react";
 export default function HomePage() {
 
     return (
-        <p>
-            this is the home page lol
-        </p>
+        <div 
+        className="min-h-screen relative text-white" 
+        style={{background:"linear-gradient(to bottom, #0a0f1a, #1B253A, #0a0f1a)"}}>
+        
+        <p></p>
+
+        </div>
     )
 }
