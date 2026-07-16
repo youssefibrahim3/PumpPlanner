e4364e7996a495c1beb1629a06166a9bfd719c59
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-07-16 15:58:57 -0600

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,23 +1,25 @@
 import { useState, useEffect } from "react";
-
+import { useNavigate } from "react-router-dom";
 export default function HomePage() {
+    const navigate = useNavigate()
 
     return (
         <div 
         className="relative min-h-screen flex items-center justify-center overflow-hidden"
-        style={{background:"linear-gradient(to bottom, #5b5c5e, #9a280b, #5b5c5e)"}}>
+        style={{background:"linear-gradient(to bottom, #d16d54, #9a280b, #d16d54)"}}>
         
         <div className = "container mx-auto px-16 relative z-10 space-y-8">
             <h1 className="text-white font-bold text-6xl mb-4">
-                - Pump Planner -
+                Pump Planner
             </h1>
 
             <p className="text-xl text-gray-300 mb-8">
                 Begin tracking your workouts today and double your progress.
             </p>
 
-            <button className="bg-red-300 hover:bg-red-700 px-6 py-3 rounded-lg font-bold">
-                Get Started Now
+            
+            <button onClick={() => navigate('/login')} className="bg-red-300 hover:bg-red-700 px-6 py-3 rounded-lg font-bold">
+                Get Started
             </button>
         </div>
         <p></p>
