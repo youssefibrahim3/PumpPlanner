3eb00f75f9f8e2dd01186971e2f5eed2645c9706
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-07-17 22:33:37 -0600

    Update Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,2 +1,12 @@
 export default function Card()
-{}
+{
+    return (
+        <header className="bg-white-400">
+            <nav>
+                <a>asd</a>
+            </nav>
+
+        </header>
+    )
+
+}
