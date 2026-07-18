2b56a1483f7657044c09b65c3269cd3173fb9c0b
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-07-17 22:34:18 -0600

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,2 +1,11 @@
 export default function Header()
-{}
+{
+    return (
+        <header className="bg-white-400">
+            <nav>
+                <a>asd</a>
+            </nav>
+        </header>
+    )
+
+}
