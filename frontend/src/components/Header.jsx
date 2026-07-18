e0773b6df6bdb1ef3d2360dd1b8770e37d269514
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-07-17 23:08:45 -0600

    Update Header.jsx

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,9 +1,12 @@
 export default function Header()
 {
     return (
-        <header className="bg-white-400">
-            <nav>
-                <a>asd</a>
+        <header className="bg-white">
+            <nav className="mx-auto items-center flex max-w-6xl justify-center px-6 py-4">
+                <a>
+                    asd
+                </a>
+
             </nav>
         </header>
     )
