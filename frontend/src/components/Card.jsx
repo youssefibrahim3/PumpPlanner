88272f7c37dd91a0fa7a6a3194f1c7d04e95997c
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-07-18 21:00:37 -0600

    Add more styling to homepage
    I will likely end up using Card later, but I resorted to this messy code for now just for rapid prototyping of home-page design.

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,7 +1,7 @@
 export default function Card(props)
 {
     return (
-        <div className="block rounded-lg drop-shadow-red-900">
+        <div className="block rounded-lg drop-shadow-red-900 bg-gray-300">
             <p>Placeholder</p>
 
         </div>
