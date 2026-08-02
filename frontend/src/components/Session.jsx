f3d2167f9376e33b5039a0ada4fdcf75a17693f8
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 19:28:08 -0600

    Set up Session.jsx

--- frontend/src/components/Session.jsx
+++ frontend/src/components/Session.jsx
@@ -1,2 +1,9 @@
+import { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+
 export default function Session()
-{}
+{
+    const sessionId = useParams()
+}
