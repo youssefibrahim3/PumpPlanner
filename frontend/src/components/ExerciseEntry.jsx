87c5ebee4ff17ad4f410bd29a83e94b5d78f4efb
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 20:15:51 -0600

    Update ExerciseEntry.jsx

--- frontend/src/components/ExerciseEntry.jsx
+++ frontend/src/components/ExerciseEntry.jsx
@@ -7,7 +7,7 @@ export default function ExerciseEntry({exerciseData, onDelete})
         <div className="block rounded-lg drop-shadow-red-900 bg-gray-300 border-black">
             <h1>{exerciseData.id} : {exerciseData.exerciseName} - {exerciseData.sets.length} set{exerciseData.sets.length > 1 && s}</h1>
             {exerciseData.sets.map((set, i) => 
-                <h1>Set {i} : {set.reps} reps at {set.weight}lbs</h1>
+                <h1 key={i}>Set {i+1} : {set.reps} reps at {set.weight}lbs</h1>
             )}
         </div>
     );
