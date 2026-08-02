d148ecf3c9cc20d6c4c5d14a18b759bf5991111c
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 20:07:49 -0600

    Work on exercise adding and exercise display per Session

--- frontend/src/components/ExerciseEntry.jsx
+++ frontend/src/components/ExerciseEntry.jsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import Card from "./Card";
 
-export default function ExerciseEntry({data, onDelete})
+export default function ExerciseEntry({exerciseData, onDelete})
 {
     return (
         <div className="block rounded-lg drop-shadow-red-900 bg-gray-300 border-black">
-            <h1>{data.exerciseName}</h1>
-            
+            <h1>{exerciseData.id} : {exerciseData.exerciseName}</h1>
+            <h1>{exerciseData.sets.setamt} sets done of {exerciseData.sets.reps} reps of {exerciseData.sets.weight} lbs</h1>
         </div>
     );
 
--- frontend/src/components/Session.jsx
+++ frontend/src/components/Session.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ExerciseEntry from "../components/ExerciseEntry";
 
 export default function Session()
 {
@@ -19,23 +20,44 @@ export default function Session()
             date: "2026-07-30"
         })
         setExercises([
-            { id : 1, exerciseName: "Squat", sets: [{reps: 5, weight: 135}] }, //Exercise form: id (int), name (string), sets (array of objects)
-            { id : 2, exerciseName: "Bench", sets: [{reps: 12, weight: 225}]}
+            //Exercise structure: id (num), name (string), sets {reps, weight , amt of sets} (all num)
+            { id : 1, exerciseName: "Squat", sets: [{reps: 5, weight: 135, amt: 3}] }, 
+            { id : 2, exerciseName: "Bench", sets: [{reps: 12, weight: 225, amt: 3}]}
         ])
     }, [id])
 
+    function addExercise(data)
+    {
+        setExercises(
+            [
+                ...exercises,
+                data
+            ]
+        );
+
+    }
+
+    function deleteExercise(exerciseId)
+    {
+
+    }
     return (
         <div className="min-h-screen" style={{background:"linear-gradient(to bottom, #d16d54, #9a280b, #d16d54)"}}>
             <Header/>
-            <div className="justify-center mx-auto">
+            <div className="justify-center mx-auto py-24">
 
+                {exercises.map(exercise => (
+                    <ExerciseEntry key={exercise.id} exerciseData={exercise} onDelete={deleteExercise} />
+                ))}
 
-                <button onClick={() => showAddForm(true)} className="bg-red-300 hover:bg-red-700 px-6 py-3 rounded-lg font-bold">
+                <button onClick={() => showAddForm(true)} className="bg-red-300 hover:bg-red-700 px-6 py-3 rounded-lg font-bold cursor-pointer">
                     + Add Exercise
                 </button>
                 {addForm && 
                 <div>
-                    <p>content goes here</p>
+                    <form onSubmit={addExercise}>
+                        <input type="text"></input>
+                    </form>
                 </div>
                 }
             </div>
