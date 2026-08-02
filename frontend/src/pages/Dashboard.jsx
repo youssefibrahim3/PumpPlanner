830059521e1c37e86cc65a589c0b353434199cbb
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 19:19:44 -0600

    Setup Dashboard.jsx
    Initial variable setup

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,2 +1,10 @@
+import { useState, useEffect } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Card from "../components/Card";
+
 export default function Dashboard()
-{}
+{
+
+
+}
