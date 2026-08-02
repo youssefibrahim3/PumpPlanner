6c828da5bbff04afdf0d491c607479ce399b1275
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 19:40:06 -0600

    Update Session.jsx

--- frontend/src/components/Session.jsx
+++ frontend/src/components/Session.jsx
@@ -11,12 +11,21 @@ export default function Session()
     const [session, setSession] = useState(null) // data for this session
     const [exercises, setExercises] = useState([]) // all exercises in the session currently
     
-    return (
-        <div>
-            <Header/>
-            
-            
+    useEffect(() => {
+        setSession({
+            id: 1,
+            name: "Session1",
+            date: "2026-07-30"
+        })
+        setExercises("Squat", "Bench", "Deadlift")
+    })
 
+    return (
+        <div className="min-h-screen" style={{background:"linear-gradient(to bottom, #d16d54, #9a280b, #d16d54)"}}>
+            <Header/>
+            <div className="justify-center mx-auto">
+
+            </div>
             <Footer/>
         </div>
     );
