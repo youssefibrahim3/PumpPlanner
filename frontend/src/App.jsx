ac3d32b7ec92308a0a8215fae6edba8992eac580
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 19:21:41 -0600

    Update App.jsx
    Add in session path for implementing individual session elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
-
+import Session from './components/Session';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -15,6 +15,7 @@ function App() {
           <Route path='/' element={<HomePage/>}/>
           <Route path='/login' element={<LoginPage/>}/>
           <Route path='/dashboard' element={<Dashboard/>}/>
+          <Route path='/dashboard/session/:id' element={<Session/>}/>
       </Routes>
     </BrowserRouter>
   )
