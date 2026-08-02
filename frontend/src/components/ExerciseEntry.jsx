8540808fea64d812bb255f392475f1b71957aca8
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 19:45:44 -0600

    Update ExerciseEntry.jsx

--- frontend/src/components/ExerciseEntry.jsx
+++ frontend/src/components/ExerciseEntry.jsx
@@ -1,2 +1,13 @@
-export default function ExerciseEntry()
-{}
+import { useState } from "react";
+import Card from "./Card";
+
+export default function ExerciseEntry({data, onDelete})
+{
+    return (
+        <div className="block rounded-lg drop-shadow-red-900 bg-gray-300 border-black">
+            <h1>{data.exerciseName}</h1>
+            
+        </div>
+    );
+
+}
