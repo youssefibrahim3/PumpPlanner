409b1cb06ce5b57d153d53868dde83d16b9a3f12
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 19:25:50 -0600

    Update Dashboard.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import { useNavigate } from "react-router-dom";
 export default function Dashboard()
 {
     const navigate = useNavigate();
+    const [sessions, setSessions] = useState([]); //Array of session objects
 
     useEffect(() => {
         setSessions([
