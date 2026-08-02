5aa0ab88b3651ff82c5e317efed7845ba0756b8f
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 20:46:51 -0600

    Fix AddExerciseForm buttons
    Not definining the button's 'type' attribute caused them to default to 'submit', refreshing the page

--- frontend/src/components/AddExerciseForm.jsx
+++ frontend/src/components/AddExerciseForm.jsx
@@ -15,6 +15,11 @@ export default function AddExerciseForm({onSubmit, onCancel})
         )
     }
 
+    function updateSet(index, field, value) {
+        setSets(sets.map((s, i) => i === index ? { ...s, [field]: value } : s))
+    }
+
+
     function handleSubmit(e) 
     {
         e.preventDefault()
@@ -26,19 +31,24 @@ export default function AddExerciseForm({onSubmit, onCancel})
     }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type="text" placeholder="Exercise Name"></input>
-            <button onClick={addSet} className="bg-red-300 hover:bg-red-700 px-6 py-3 rounded-lg font-bold cursor-pointer">
+
+            <button type="button" onClick={addSet} className="bg-red-300 hover:bg-red-700 px-6 py-3 rounded-lg font-bold cursor-pointer">
                 + Add Set
             </button>
+
             {sets.map((set, i) => {
                 <div className="rounded-lg bg-red-300 my-3 py-3">
                     <h1>Set {i + 1}</h1>
-                    <input type="number" placeholder="Reps" value={set.reps} required></input>
-                    <input type="number" placeholder="Weight" value={set.weight} required></input>
+                    <input type="number" placeholder="Reps" value={set.reps} required onChange={(e) => updateSet(i, "reps", e.target.value)}></input>
+                    <input type="number" placeholder="Weight" value={set.weight} required onChange={(e) => updateSet(i, "reps", e.target.value)}></input>
                 </div>
             })}
 
+            <button type="submit" className="bg-red-300 hover:bg-red-700 px-6 py-3 rounded-lg font-bold cursor-pointer">
+                Add
+            </button>
         </form>
     )
 }
