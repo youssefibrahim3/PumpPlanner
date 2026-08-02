e963bcecf91c1ed92dc4e5324f14b032841ed065
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 20:43:25 -0600

    Update AddExerciseForm.jsx

--- frontend/src/components/AddExerciseForm.jsx
+++ frontend/src/components/AddExerciseForm.jsx
@@ -7,7 +7,12 @@ export default function AddExerciseForm({onSubmit, onCancel})
     
     function addSet() 
     {
-
+        setSets(
+            [
+                ...sets,
+                {reps: "", weight: ""}
+            ]
+        )
     }
 
     function handleSubmit(e) 
@@ -19,10 +24,21 @@ export default function AddExerciseForm({onSubmit, onCancel})
             sets: sets.map(set => ({reps: Number(set.reps), weight: Number(set.weight)}) )
         })
     }
+
     return (
         <form>
-            <input type="text"></input>
-            <input type="time"></input>
+            <input type="text" placeholder="Exercise Name"></input>
+            <button onClick={addSet} className="bg-red-300 hover:bg-red-700 px-6 py-3 rounded-lg font-bold cursor-pointer">
+                + Add Set
+            </button>
+            {sets.map((set, i) => {
+                <div className="rounded-lg bg-red-300 my-3 py-3">
+                    <h1>Set {i + 1}</h1>
+                    <input type="number" placeholder="Reps" value={set.reps} required></input>
+                    <input type="number" placeholder="Weight" value={set.weight} required></input>
+                </div>
+            })}
+
         </form>
     )
 }
