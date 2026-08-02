2aebe0ff84e82f3918ef3341e942f8e5d3e756dd
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-01 19:36:33 -0600

    Update Session.jsx

--- frontend/src/components/Session.jsx
+++ frontend/src/components/Session.jsx
@@ -6,4 +6,19 @@ import Footer from "./Footer";
 export default function Session()
 {
     const sessionId = useParams()
+    const navigate = useNavigate()
+
+    const [session, setSession] = useState(null) // data for this session
+    const [exercises, setExercises] = useState([]) // all exercises in the session currently
+    
+    return (
+        <div>
+            <Header/>
+            
+            
+
+            <Footer/>
+        </div>
+    );
+
 }
