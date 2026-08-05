1045e54cc5528e4361ce49f96a70673e28754ad7
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-05 12:19:14 -0600

    Update schemas.py

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,23 +1,41 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
+
+class SetCreate(BaseModel):
+    reps : int
+    weight : float
+
+class SetRead(BaseModel):
+    id : int
+    reps : int
+    weight : float
+
+    model_config = ConfigDict(from_attributes=True)
+
+
+class ExerciseCreate(BaseModel):
+    exerciseName : str
+    unit : str
+    sets : list[SetCreate]
+
+class ExerciseRead(BaseModel):
+    id : int
+    exerciseName : str
+    unit : str
+    sets : list[SetRead]
+
+    model_config = ConfigDict(from_attributes=True)
 
 class SessionCreate(BaseModel):
     name : str
     date : str
 
 class SessionRead(BaseModel):
-    pass
+    id : int
+    name : str
+    date : str
+    exercises : list[ExerciseRead]
 
-class ExerciseCreate(BaseModel):
-    pass
-
-class ExerciseRead(BaseModel):
-    pass
-
-class SetCreate(BaseModel):
-    pass
-
-class SetRead(BaseModel):
-    pass
+    model_config = ConfigDict(from_attributes=True)
 
 class UserCreate(BaseModel):
     username : str
@@ -26,5 +44,7 @@ class UserCreate(BaseModel):
 class UserRead(BaseModel):
     id : int
     username : str
+
+    model_config = ConfigDict(from_attributes=True)
 
 
