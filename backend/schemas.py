2d24d1d894ace6125836487b4994fd5b9e14cfe5
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-05 12:06:22 -0600

    Set up Pydantic API schemas

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,1 +1,30 @@
 from pydantic import BaseModel
+
+class SessionCreate(BaseModel):
+    name : str
+    date : str
+
+class SessionRead(BaseModel):
+    pass
+
+class ExerciseCreate(BaseModel):
+    pass
+
+class ExerciseRead(BaseModel):
+    pass
+
+class SetCreate(BaseModel):
+    pass
+
+class SetRead(BaseModel):
+    pass
+
+class UserCreate(BaseModel):
+    username : str
+    password : str
+
+class UserRead(BaseModel):
+    id : int
+    username : str
+
+
