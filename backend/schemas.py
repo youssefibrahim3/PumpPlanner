1dd1470011fa86a4247c44a9f5dbea4d47506718
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-06 16:32:53 -0600

    Add field validators to username and password

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, field_validator
 
 class SetCreate(BaseModel):
     reps : int
@@ -40,6 +40,19 @@ class SessionRead(BaseModel):
 class UserCreate(BaseModel):
     username : str
     password : str
+
+    @field_validator('username')
+    @classmethod
+    def validate_username(cls, value : str):
+        if ' ' in value:
+            raise ValueError("Username cannot contain spaces")
+        return value
+
+    @field_validator('password')
+    def validate_password(cls, value : str):
+        if len(value) < 6:
+            raise ValueError("Password must be at least 6 characters long")
+        return value
 
 class UserRead(BaseModel):
     id : int
