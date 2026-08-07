ac509817b25e74d6294662ac4ec986b019a314dc
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-07 15:24:57 -0600

    Update Card.jsx

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ children, onClick }) {
     return (
-        <div onClick={onClick} className="block rounded-lg drop-shadow-red-900 bg-gray-300 cursor-pointer">
+        <div onClick={onClick} className="block rounded-lg drop-shadow-red-900 bg-gray-300 cursor-pointer px-5 py-5">
             {children}
         </div>
     )
