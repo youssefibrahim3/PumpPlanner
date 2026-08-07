321386be2d21fba927a1372678fa307c143b9042
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-07 12:19:46 -0600

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,8 +1,10 @@
 export default function Footer()
 {
     return (
-        <footer className="bg-white w-screen fixed bottom-0">
-            <p className="text-center">Created by Youssef Ibrahim</p>
+        <footer className="bg-white/95 z-50 w-screen fixed bottom-0 backdrop-blur-md border-gray-200">
+            <p className="text-center text-sm text-gray-500 py-4">
+                Built by <span className="text-red-500 font-bold">Youssef Ibrahim</span>
+                </p>
         </footer>
     )
 }
