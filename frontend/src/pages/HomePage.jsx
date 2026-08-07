21dc854d743ef5b4a9e1eedc59d9a0f947606e72
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-07 12:15:26 -0600

    Update HomePage.jsx

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -29,22 +29,22 @@ export default function HomePage() {
             </button>
 
             <div className="grid grid-cols-3 gap-5">
-                <div className="block rounded-xl drop-shadow-red-900 bg-gray-300 py-20 drop-shadow-lg">
+                <div className="block rounded-xl drop-shadow-red-900 bg-gray-300 py-20 drop-shadow-lg px-5">
                     <h2 className="text-black py-5 text-3xl text-center drop-shadow-sm drop-shadow-red-600">Create workout sessions</h2>
                     <p className="text-center text-black leading-loose">
                         Create individual workout sessions to represent each time you go out and lift. Organize them as you please and create reusable routines
                     </p>
                 </div>
-                <div className="block rounded-xl drop-shadow-red-900 bg-gray-300 py-20 drop-shadow-lg">
+                <div className="block rounded-xl drop-shadow-red-900 bg-gray-300 py-20 drop-shadow-lg px-5">
                     <h2 className="text-black py-5 text-3xl text-center drop-shadow-sm drop-shadow-red-600">Log your lifts</h2>
                     <p className="text-center text-black leading-loose">
                         Log any type of workout into your sessions and track your sets, weight, and reps per set. Track break times and optimize your gains.
                     </p>
                 </div>
-                <div className="block rounded-xl drop-shadow-red-900 bg-gray-300 py-20 drop-shadow-lg">
+                <div className="block rounded-xl drop-shadow-red-900 bg-gray-300 py-20 drop-shadow-lg px-5">
                     <h2 className="text-black py-5 text-3xl text-center drop-shadow-sm drop-shadow-red-600">Gain more muscle</h2>
                     <p className="text-center text-black leading-loose">
-                        Gym tracking is proven to be highly evffective for building muscle and encouraging progressive overload. Begin tracking today to ensure your muscles are consistently challenged, and your goals are consistently hit. 
+                        Gym tracking is proven to be highly effective for building muscle and encouraging progressive overload. Begin tracking today to ensure your muscles are consistently challenged, and your goals are consistently hit. 
                     </p>
                 </div>
             </div>
