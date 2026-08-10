dce376a4374845eff9b76a0a7bd45a9e8c1294bd
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-09 22:13:27 -0600

    Update Session.jsx

--- frontend/src/components/Session.jsx
+++ frontend/src/components/Session.jsx
@@ -130,7 +130,7 @@ export default function Session()
             
             <div className="justify-center mx-auto py-24">
                 <a href="/dashboard">
-                    <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <svg class="w-9 h-9 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4"/>
                     </svg>
                 </a>
