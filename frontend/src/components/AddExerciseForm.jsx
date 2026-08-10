77b5dd742456c2065d2dddc79ea99c0b2d00542a
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-08-09 22:03:03 -0600

    Update AddExerciseForm.jsx

--- frontend/src/components/AddExerciseForm.jsx
+++ frontend/src/components/AddExerciseForm.jsx
@@ -11,7 +11,7 @@ export default function AddExerciseForm({onSubmit, onCancel})
         setSets(
             [
                 ...sets,
-                {reps: "", weight: ""}
+                {reps: reps, weight: weight}
             ]
         )
     }
